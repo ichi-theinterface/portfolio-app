18282d60337b869c026fe854e572c9d2d66737f4
Ichiro Home DT <ichi.theinterface@gmail.com>
2022-05-29 12:52:55 -0700

    edit css for greeting js

--- src/component/Greeting.js
+++ src/component/Greeting.js
@@ -4,9 +4,9 @@ import header from '../App.css';
 const Greeting = () => (
     <div className="greeting">
      <p>
-      <h3>Hi, my name is </h3>
+      <h3 className="HiMyNameIs">Hi, my name is </h3>
       <h1 className="NameinGreeting ">Ichiro Yamasaki.</h1>
-      <h1>I scalle network infrastructure of cloud service provider.</h1>
+      <h1 className="OneLineDescription">I scalle network infrastructure of cloud service provider.</h1>
       </p>
     </div>
   );
