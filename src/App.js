f5adda3a9639c5fd7b64c8a3b1d6515ea7c5a6fc
Ichiro Home DT <ichi.theinterface@gmail.com>
2022-05-29 14:58:12 -0700

    edit sidebar on 05-29

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import Header from './component/Header';
 import SocialMedia from './component/SocialMedia';
 import Greeting from './component/Greeting'
 import BriefIntroduction from './component/BriefIntroduction'
+import SideBar from './component/sidebar';
 
 function App() {
   return (
     <div className="canvas">
       <Header />
+      <SideBar />
       <Greeting />
       <BriefIntroduction />
-      <SocialMedia />
       </div>
   );
 }
