ddd1294f6532c1aedf4d924411ca0851dd4e4169
Ichiro Home DT <ichi.theinterface@gmail.com>
2022-05-30 20:28:03 -0700

    edit brief introduction 05-30

--- src/component/BriefIntroduction.js
+++ src/component/BriefIntroduction.js
@@ -4,8 +4,9 @@ const BriefIntroduction = () => (
     <div className="BriefIntroduction">
       <h3>A network tech in a cloud provider</h3>
       <p>
-          Stay hungy and curious about the possibility of technology. <br/>
-          What I love about IT is that more I learn about it, more things I find that I do not know!! 
+          Stay hungy and curious about the possibility of technology.<br/>
+          What I love about IT is that more I learn about it,<br/>
+          more things I find that I do not know!! 
       </p>
     </div>
   );
