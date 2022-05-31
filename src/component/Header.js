d92a6f68adf5c3a4c0cdce10f63272a0920ed1ce
Ichiro Home DT <ichi.theinterface@gmail.com>
2022-05-30 23:49:00 -0700

    adjusting the header layout 05-30

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,26 +4,36 @@ import { Link } from "react-router-dom";
 
 const Header = () => (
     <div className="header">
-       <Link to="/">
-         <button type="button">
-              Home
-         </button>
-       </Link>
-        <Link to="/Accomplishment">
-         <button type="button">
-              Accomplishment
-         </button>
-       </Link>
-       <Link to="/Blogs">
-         <button type="button">
-              Blogs
-         </button>
-       </Link>
-       <Link to="/AboutthisApp">
-         <button type="button">
-              About this app
-         </button>
-       </Link>
+      <div className='HeaderButtonFrame'>
+        <div className='HeaderButton'>
+          <Link to="/">
+            <button type="button">
+                 Home
+            </button>
+          </Link>
+         </div>
+        <div className='HeaderButton'>
+          <Link to="/Accomplishment">
+           <button type="button">
+                Accomplishment
+           </button>
+          </Link>
+         </div>
+        <div className='HeaderButton'>
+          <Link to="/Blogs">
+            <button type="button">
+                Blogs
+             </button>
+           </Link>
+         </div>
+        <div className='HeaderButton'>
+          <Link to="/AboutthisApp">
+            <button type="button">
+                 About this app
+            </button>
+          </Link>
+         </div>
+       </div>
     </div>
   );
 
