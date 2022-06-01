dc218e4a23689bd2acc0e3ffa79f94af0d7550b7
Ichiro Yamasaki <ichi.theinterface@gmail.com>
2022-05-31 17:10:21 -0700

    styling header 05-31

--- src/component/Experience.js
+++ src/component/Experience.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 const Experience = () => (
     <div className='Experience'>
-        <h2>Experience</h2>
+        <h2 className='WhereIWorked'>Where I've worked</h2>
+        <div>
+            Amazon AWS
+        </div>
+        <div>
+            SYSCOM GLOBAL SOLUIONS INC
+        </div>
+        <div>
+            International Education Center
+        </div>
     </div>
   );
 
