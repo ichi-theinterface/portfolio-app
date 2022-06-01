b0109c2d4b5927125c7a4c24e8e5906049e6252c
Ichiro Yamasaki <ichi.theinterface@gmail.com>
2022-05-31 17:27:37 -0700

    need to improve the .frame(css) and experience sec

--- src/component/Experience.js
+++ src/component/Experience.js
@@ -4,7 +4,13 @@ const Experience = () => (
     <div className='Experience'>
         <h2 className='WhereIWorked'>Where I've worked</h2>
         <div>
-            Amazon AWS
+            <h3>Amazon AWS</h3>
+            <h4>Network Deploy Technician</h4>
+            <li>
+                <p>Scale up the existing network infrastructure by deploying new devices.</p>
+                <p>Utilizing the automation software and being compliant with policies to eliminate or minize customer impact.</p>
+                <p>Communicate with project stake-holders to remove the obstacles.</p>
+            </li>
         </div>
         <div>
             SYSCOM GLOBAL SOLUIONS INC
